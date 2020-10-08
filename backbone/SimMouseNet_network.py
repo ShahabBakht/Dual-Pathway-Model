b577d42b066e225f218887ef130ec369205ebb87
Shahab Bakhtiari <shahab@Shahabs-MacBook-Pro-2.local>
2020-10-08 10:39:38 -0400

    added SimMouseNet_network

---
+++ backbone/SimMouseNet_network.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Network:
+    def __init__(self):
+        self.AREAS_LIST = ['Retina', 'LGN', 'VISp', 'VISl', 'VISal', 'VISpm', 'VISam']
+        self.AREAS_CONNECTIVITY = [('Retina','LGN'),('LGN','VISp'),
+                            ('VISp','VISl'), ('VISp','VISal'), ('VISp','VISpm'), ('VISp','VISam')]
+
+    def create_graph(self):
+
+        self.G = nx.DiGraph()
+
+        self.G.add_nodes_from(self.AREAS_LIST)
+        self.G.add_edges_from(self.AREAS_CONNECTIVITY)
+
+    def draw_graph(self):
+
+        plt.figure(figsize=(12,12))
+        pos = nx.nx_pydot.graphviz_layout(self.G, prog='dot')
+        nx.draw(self.G, pos, with_labels=True, font_weight='bold')
+        plt.show()
+
+
